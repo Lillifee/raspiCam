28afe11237138c41387e5aabd50a7088b2d85f34
Lillifee <patrick.matt.85@gmail.com>
2021-04-16 12:09:35 +0200

    Fixed slider styling for android

--- src/site/components/styled/Slider.tsx
+++ src/site/components/styled/Slider.tsx
@@ -7,16 +7,21 @@ export interface SliderProps {
 
 const SliderStyled = styled.input<SliderProps>`
   flex: 1;
+  min-height: 25px;
   appearance: none;
   outline: none;
-  border-radius: 3px;
-  margin: 0.5em 0;
-  height: 8px;
-  background: ${(props) => props.theme.Background};
+  background: transparent;
 
   // Disable range pointer events for mobile devices
   @media only screen and (max-width: 1000px) {
     pointer-events: none;
+  }
+  ::-webkit-slider-runnable-track {
+    width: 100%;
+    height: 8px;
+    cursor: pointer;
+    background: ${(props) => props.theme.Background};
+    border-radius: 3px;
   }
 
   ::-webkit-slider-thumb {
@@ -29,6 +34,7 @@ const SliderStyled = styled.input<SliderProps>`
       props.unset ? props.theme.Background : props.theme.PrimaryBackground};
     border: 1px solid ${(props) => props.theme.Border};
     border-radius: 15px;
+    margin-top: -7px;
   }
 
   &:disabled {
